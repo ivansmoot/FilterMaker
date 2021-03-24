7c30818bec7900db7a6659bbf264439d9615b292
ivansmoot <ivansmoot@163.com>
2021-03-24 13:43:30 +0800

    修正desc文件生成内容

--- python_scripts/utils/filter_desc_maker.py
+++ python_scripts/utils/filter_desc_maker.py
@@ -6,13 +6,13 @@ def desc_maker(filter_id, filter_name, filter_data_md5):
     # 由于format是通过 { 去实现匹配替换的,但是我们的第一个左花括号是不需要被替换的,所以要再包一层括号,作为转义,而不是使用\进行转义
     # python的坑是真的有点多
     content = '''{{
-    "template_fw": 1,
-    "template_id": {},
-    "template_name": {},
-    "template_des": {},
-    "template_data": {},
-    "template_is_encrypt": false,
-    "template_data_md5": {}
+    "template_fw":1,
+    "template_id":"{}",
+    "template_name":"{}",
+    "template_des":"{}",
+    "template_data":"{}",
+    "template_is_encrypt":false,
+    "template_data_md5":"{}"
 }}'''
     filter_desc_content = content.format(filter_id, filter_name, filter_name, filter_id + '.data', filter_data_md5)
     # 存文件
